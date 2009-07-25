b55ee77f6623ac34debbe626bc9186cdb43aa659
AndreFMiller <AndreFMiller@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-07-25 13:03:08 +0000

    Initial commit of MediaNav server

---
+++ trunk/server/tv/management/commands/tv_importshow.py
@@ -0,0 +1,95 @@
+from django.core.management.base import LabelCommand
+from django.conf import settings
+from tv.thetvdbapi import TheTVDB
+from tv.models import Show, Episode
+import urllib
+import os
+
+class Command(LabelCommand):
+    def handle_label(self, label, **options):
+        print "Querying TheTVDB..."
+        tvdbapi = TheTVDB(settings.TVDBAPI_KEY)
+        show_id = label
+        result = tvdbapi.get_show_and_episodes(show_id)
+        if result:
+            tvdbshow, tvdbepisodes = result
+            print "Show.name = %s" % (tvdbshow.name,) 
+            try:
+                show = Show.objects.get(tvdb_showid=tvdbshow.id)
+                print "Updating existing show"
+            except Show.DoesNotExist:
+                print "Creating new show"
+                show = Show(tvdb_showid=tvdbshow.id)
+            show.name = tvdbshow.name
+            show.overview = tvdbshow.overview
+            show.genre = str(tvdbshow.genre[0]) # FIXME This is an array
+            show.network = tvdbshow.network
+            show.content_rating = tvdbshow.content_rating
+            show.runtime = tvdbshow.runtime
+            show.status = tvdbshow.status
+            show.first_aired = tvdbshow.first_aired
+            show.airs_day = tvdbshow.airs_day
+            show.airs_time = tvdbshow.airs_time
+            show.tvdb_language = tvdbshow.language
+            if tvdbshow.rating:
+                show.tvdb_rating = tvdbshow.rating
+            show.tvdb_banner_url = tvdbshow.banner_url
+            show.tvdb_fanart_url = tvdbshow.fanart_url
+            show.tvdb_poster_url = tvdbshow.poster_url
+            show.tvdb_last_updated = tvdbshow.last_updated
+            show.save()
+            # Download the images for the show
+            filename = "/home/andre/projects/medianav/server/medianav/tv/media/img/banner/%d.jpg" % show.tvdb_showid
+            if not os.access(filename, os.F_OK):
+                print "Downloading banner image"
+                urllib.urlretrieve(show.tvdb_banner_url, filename)
+            filename = "/home/andre/projects/medianav/server/medianav/tv/media/img/poster/%d.jpg" % show.tvdb_showid
+            if not os.access(filename, os.F_OK):
+                print "Downloading poster image"
+                urllib.urlretrieve(show.tvdb_poster_url, filename)
+            filename = "/home/andre/projects/medianav/server/medianav/tv/media/img/fanart/%d.jpg" % show.tvdb_showid
+            if not os.access(filename, os.F_OK):
+                print "Downloading fanart image"
+                urllib.urlretrieve(show.tvdb_fanart_url, filename)
+            # Add / Update episodes from tvdb to medianav 
+            for tvdbepisode in tvdbepisodes:
+                update = 0
+                update_fields = ""
+                try:
+                    print "Update: s%02de%02d %s" % (int(tvdbepisode.season_number), int(tvdbepisode.episode_number), tvdbepisode.name)
+                    episode = Episode.objects.get(tvdb_episodeid=tvdbepisode.id)
+                except Episode.DoesNotExist:
+                    episode = Episode(tvdb_episodeid=tvdbepisode.id)
+                    print "Create: s%02de%02d %s" % (int(tvdbepisode.season_number), int(tvdbepisode.episode_number), tvdbepisode.name)
+                if episode.show != show:
+                    episode.show = show
+                    update = 1
+                    update_fields += " show"
+                if episode.name != tvdbepisode.name:
+                    episode.name = tvdbepisode.name
+                    update = 1
+                    update_fields += " name"
+                episode.overview = tvdbepisode.overview
+                episode.season_number = tvdbepisode.season_number
+                episode.episode_number = tvdbepisode.episode_number
+                episode.director = tvdbepisode.director
+                episode.guest_stars = tvdbepisode.guest_stars
+                episode.production_code = tvdbepisode.production_code
+                episode.writer = tvdbepisode.writer
+                episode.first_aired = tvdbepisode.first_aired
+                episode.tvdb_language = tvdbepisode.language
+                if tvdbepisode.rating:
+                    episode.tvdb_rating = tvdbepisode.rating
+                episode.tvdb_image = tvdbepisode.image
+                episode.tvdb_last_updated = tvdbepisode.last_updated
+                episode.save()
+                if update:
+                    print "Update: s%02de%02d %s [%s]" % (int(tvdbepisode.season_number), int(tvdbepisode.episode_number), tvdbepisode.name, update_fields),
+            # Remove any episodes that are no longer in tvdb list
+            for episode in show.episode_set.all():
+                if episode.tvdb_episodeid not in (int(e.id) for e in tvdbepisodes):
+                    print "Delete: s%02de%02d %s" % (episode.season_number, episode.episode_number, episode.name)
+                    episode.delete()
+        else:
+            print "Unable to retrieve information for show_id = %s" % (show_id,)
+        
---
+++ trunk/server/tv/management/commands/tv_scanall.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+# Scans directories for TV episodes
+
+import os
+from tv.parsevideofile import parsevideofile
+from django.core.management.base import NoArgsCommand 
+from tv.models import Show, Episode, VideoFile, AlternateShowName
+
+TVDIR='/data/tv' # FIXME Get this information from the database
+
+class Command(NoArgsCommand):
+    def handle(self, **options):
+        verbose = False    
+        for dirname, dirnames, filenames in os.walk(TVDIR):
+            for filename in filenames:
+                name=os.path.join(dirname, filename).replace(TVDIR,'',1)
+                name = name.lstrip(os.sep)
+                parsed=parsevideofile(name)
+                if parsed is None:
+                    print "Cannot parse file %s" % (name,)
+                    continue
+                if 'name' not in parsed:
+                    print "Cannot parse show name from %s" % (name,)
+                    continue
+                # Try to match it to a show, if we have a match we can add
+                # it to the database
+                try:
+                    show = Show.objects.get(name__iexact=parsed['name'])
+                except Show.DoesNotExist:
+                    # Try to find the show from the alternate names 
+                    try:
+                        show = AlternateShowName.objects.get(name__iexact=parsed['name']).show
+                    except AlternateShowName.DoesNotExist:
+                        print "Cannot match Show %s" % (parsed['name'],)
+                        continue
+                # Check if this file is already in the database
+                try:
+                    videofile = VideoFile.objects.get(name__iexact=name)
+                    if verbose: print "Updating %s" % (name,)
+                    videofile.name=name # Update name in case case changed
+                    videofile.show=show # Update show it relates to
+                except VideoFile.DoesNotExist:
+                    if verbose: print "Creating %s" % (name,)
+                    videofile = VideoFile(name=name, show=show)
+                    videofile.save() # Must save it before we can add many-to-many later
+                # Try to match it to an episode
+                # We can only do this is both season and episode numbers exist
+                if ('season' in parsed) and (parsed['season'] is not None) and ('episode' in parsed) and (parsed['episode'] is not None):
+                    startep_number = int(parsed['episode'])
+                    season_number = int(parsed['season'])
+
+                    # Check if it covers more than one episode
+                    if ('endep' in parsed) and (parsed['endep'] is not None):
+                        endep_number = int(parsed['endep'])
+                    else:
+                        endep_number = startep_number
+                    # Sanity check to make sure the endep is valid
+                    if endep_number < startep_number:
+                        endep_number = startep_number
+                    # Loop through the episodes and add them if we find them
+                    for ep_number in range(startep_number, endep_number+1):
+                        # Because of data inconsistencies in TheTVDB's data there might be
+                        # duplicates. Assign this to all occurrences of the episode in the DB
+                        # FIXME This should be picked up during a consistency check and
+                        # reported to TheTVDB to clean up
+                        episodes = Episode.objects.filter(show=show, season_number=season_number, episode_number = ep_number)
+                        for episode in episodes:
+                            if verbose: print "    Linking episode ", episode
+                            videofile.episodes.add(episode)
+
+                videofile.save()
+
+
+
