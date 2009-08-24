7c2da22d556871034ef3d026dc99bc85bb1f6e39
AndreFMiller <AndreFMiller@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-08-24 14:12:50 +0000

    Checked in imdb integration

---
+++ trunk/server/movies/management/commands/movie_imdb_search.py
@@ -0,0 +1,12 @@
+from django.core.management.base import LabelCommand
+from movies.utils import import_imdb
+from movies.models import Movie
+from movies.imdb import IMDb
+
+class Command(LabelCommand):
+    def handle_label(sself, label, **options):
+        """ Searches for an imdb movie by title """
+        imdb = IMDb('http', useModule='BeautifulSoup')
+        s=imdb.search_movie(label)
+        for r in s:
+            print "%s - %s (%s)" % (r.movieID, r['title'], r['year'])
