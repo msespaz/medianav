9b48e10a0e47b248eafd9425b2362372a42cd916
AndreFMiller <AndreFMiller@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-08-30 18:01:31 +0000

    Removed django_restapi, no longer used

--- trunk/server/urls.py
+++ trunk/server/urls.py
@@ -14,6 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/(.*)', admin.site.root),
-    (r'^tv/', include('tv.urls')),
-    (r'^movies/', include('movies.urls')),
+    (r'^/*tv/', include('tv.urls')),
+    (r'^/*movies/', include('movies.urls')),
 )
