73b1823f9d0643f9151b18b3aeb1d9596e0f081d
AndreFMiller <AndreFMiller@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-08-30 17:59:50 +0000

    Removed dependancy on django_restapi. Now uses the built-in django json serializer

--- trunk/server/tv/views.py
+++ trunk/server/tv/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
 from django.shortcuts import render_to_response
-from tv.models import Show, Episode
+from django.http import HttpResponse
+from tv.models import Show, Episode, VideoFile
+from django.core import serializers
 import datetime
 
 def shows_list(request):
@@ -20,3 +22,18 @@ def missing_episodes(request):
     # Only pulls up a list for the last year and excludes specials
     episodes = Episode.objects.filter(season_number__gt=0, first_aired__gt=datetime.date.today()-datetime.timedelta(weeks=52), videofile__isnull=True).order_by('first_aired')
     return render_to_response('missing_episodes.html', {'episodes' : episodes, })
+
+def json_show_episodes(request, show_id):
+    """ Returns a json result with a list of episodes for a particular show """
+    episodes = Episode.objects.filter(show__id=show_id)
+    return HttpResponse(serializers.serialize('json', episodes), content_type='application/json')
+
+def json_shows_list(request):
+    """ Returns a list of shows in json """
+    shows = Show.objects.all()
+    return HttpResponse(serializers.serialize('json', shows), content_type='application/json')
+
+def json_episode_videofiles(request, episode_id):
+    """ Returns a json result with a list of episodes for a particular show """
+    videofiles = VideoFile.objects.filter(episodes__id=episode_id)
+    return HttpResponse(serializers.serialize('json', videofiles), content_type='application/json')
