64e5d10831cd0b39cd9c88fcd1c639ae56068588
AndreFMiller <AndreFMiller@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-08-31 15:11:47 +0000

    Updated views to use RequestContext and locals()

--- trunk/server/tv/views.py
+++ trunk/server/tv/views.py
@@ -1,27 +1,28 @@
 # Create your views here.
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.template import RequestContext
 from tv.models import Show, Episode, VideoFile
 from django.core import serializers
 import datetime
 
 def shows_list(request):
     shows = Show.objects.all()
-    return render_to_response('shows_list.html', {'shows' : shows, })
+    return render_to_response('shows_list.html', locals(), context_instance=RequestContext(request))
 
 def show_detail(request, show_id):
     show = Show.objects.get(pk=show_id)
-    return render_to_response('show_detail.html', {'show' : show, })
+    return render_to_response('show_detail.html', locals(), context_instance=RequestContext(request))
 
 def episode_detail(request, episode_id):
     episode = Episode.objects.get(pk=episode_id)
-    return render_to_response('episode_detail.html', {'episode' : episode, })
+    return render_to_response('episode_detail.html', locals(), context_instance=RequestContext(request))
 
 def missing_episodes(request):
     # A list of episodes for which there are no files
     # Only pulls up a list for the last year and excludes specials
     episodes = Episode.objects.filter(season_number__gt=0, first_aired__gt=datetime.date.today()-datetime.timedelta(weeks=52), videofile__isnull=True).order_by('first_aired')
-    return render_to_response('missing_episodes.html', {'episodes' : episodes, })
+    return render_to_response('missing_episodes.html', locals(), context_instance=RequestContext(request))
 
 def json_show_episodes(request, show_id):
     """ Returns a json result with a list of episodes for a particular show """
