f5dcbbceaad59ccf98a4a8a1f698cd5edf65b083
AndreFMiller <AndreFMiller@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-08-31 15:09:39 +0000

    Updated views to use a RequestContext and locals()

--- trunk/server/movies/views.py
+++ trunk/server/movies/views.py
@@ -1,5 +1,6 @@
 # Create your views here.
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.http import HttpResponse
 from django.core import serializers
 from movies.models import *
@@ -7,38 +8,34 @@ import datetime
 
 def movies_list(request):
     movies = Movie.objects.all()
-    return render_to_response('movies_list.html', {'movies' : movies, })
+    return render_to_response('movies_list.html', locals(), context_instance=RequestContext(request))
 
 def movie_detail(request, movie_id):
     movie = Movie.objects.get(pk=movie_id)
     directors = Cast.objects.filter(movie__id=movie_id, bill_order__lt=15, job='director').order_by('bill_order')
     actors = Cast.objects.filter(movie__id=movie_id, bill_order__lt=15, job='actor').order_by('bill_order')
     writers = Cast.objects.filter(movie__id=movie_id, bill_order__lt=15, job='writer').order_by('bill_order')
-    return render_to_response('movie_detail.html', {'movie' : movie, 'actors' : actors, 'directors' : directors, 'writers' : writers })
+    return render_to_response('movie_detail.html', locals(), context_instance=RequestContext(request))
 
 def genre_detail(request, genre_id):
     genre = Genre.objects.get(pk=genre_id)
     movies = genre.movie_set.all()
-    return render_to_response('genre_detail.html', {'genre' : genre, 'movies' : movies })
-    return None
+    return render_to_response('genre_detail.html', locals(), context_instance=RequestContext(request))
 
 def person_detail(request, person_id):
     person = Person.objects.get(pk=person_id)
     cast = Cast.objects.filter(person__id=person_id).order_by('movie__year')
-    return render_to_response('person_detail.html', {'person' : person, 'cast' : cast })
-    return None
+    return render_to_response('person_detail.html', locals(), context_instance=RequestContext(request))
 
 def company_detail(request, company_id):
     company = Company.objects.get(pk=company_id)
     movies = company.movie_set.all()
-    return render_to_response('company_detail.html', {'company' : company, 'movies' : movies })
-    return None
+    return render_to_response('company_detail.html', locals(), context_instance=RequestContext(request))
 
 def country_detail(request, country_id):
     country = Country.objects.get(pk=country_id)
     movies = country.movie_set.all()
-    return render_to_response('country_detail.html', {'country' : country, 'movies' : movies })
-    return None
+    return render_to_response('country_detail.html', locals(), context_instance=RequestContext(request))
 
 def json_movies_list(request):
     """ Returns a list of movies in json """
