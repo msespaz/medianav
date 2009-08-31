99c9ae6f207afb14ff64eca8fab7f4ee75e2cb88
AndreFMiller <AndreFMiller@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-08-31 15:15:11 +0000

    Added some user login/logout pages and displaying user in template

--- trunk/server/urls.py
+++ trunk/server/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import *
 
+# Django authentication
+from django.contrib.auth.views import login, logout
+
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -11,9 +14,15 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
     # to INSTALLED_APPS to enable admin documentation:
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
     # Uncomment the next line to enable the admin:
     (r'^admin/(.*)', admin.site.root),
+
+    # Custom app views
     (r'^/*tv/', include('tv.urls')),
     (r'^/*movies/', include('movies.urls')),
+
+    # Authentication views
+    (r'^accounts/login/$', login),
+    (r'^accounts/logout/$', logout),
+    (r'^accounts/profile/$', "tv.views.shows_list"),
 )
