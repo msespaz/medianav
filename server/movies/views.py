a17f18d060e2e1696635a70db202bf0609f238a8
AndreFMiller <AndreFMiller@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-09-15 08:58:08 +0000

    CSV download for movie list

--- server/movies/views.py
+++ server/movies/views.py
@@ -6,6 +6,38 @@ from django.core import serializers
 from movies.models import *
 from django.db.models import Count
 import datetime
+import csv, cStringIO, codecs
+
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([s.encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
+
 
 def search(request):
     if 'q' in request.GET and request.GET['q']:
@@ -15,6 +47,21 @@ def search(request):
     else:
         query = 'No search criteria specified' 
     return render_to_response('search_result.html', locals(), context_instance=RequestContext(request))
+
+def csv_list(request):
+    """ Renders a csv list of all movies """
+    response = HttpResponse(mimetype='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=movielist.csv'
+
+    # Create CSV writer using the HttpResponse as the 'file'
+    writer = UnicodeWriter(response)
+    #writer = csv.writer(response)
+    writer.writerow(['Name', 'Year', 'Quality'])
+    for d in VideoDirectory.objects.all():
+        m = d.movie
+        v = d.main_video()
+        writer.writerow([m.title, m.year.__str__(), d.get_size().__str__(), v.video_def(), v.audio_chan_name(), v.extension_name(), v.video_name(), v.audio_name()])
+    return response
 
 def movies_list(request):
     movies = Movie.objects.all()
