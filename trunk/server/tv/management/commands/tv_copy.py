5abe9e968e21c44b406678005019e352a6278f72
AndreFMiller <AndreFMiller@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-10-07 09:15:32 +0000

    Management command to facilitate copying unwatched episodes

---
+++ trunk/server/tv/management/commands/tv_copy.py
@@ -0,0 +1,34 @@
+from django.core.management.base import LabelCommand
+from tv.models import *
+from django.contrib.auth.models import User
+import os
+
+SOURCEPATH="/data/tv"
+TARGETPATH="/data/temp/copy_tv"
+
+class Command(LabelCommand):
+    def handle_label(self, label, **options):
+        username=label
+        user = User.objects.get(username=username)
+        fav_shows = user.show_set.all()
+        unseen_list = []
+        for show in fav_shows:
+            episodes = show.episode_set.filter(videofile__isnull=False).exclude(seen_by=user)
+            for episode in episodes:
+                videopath = episode.videofile_set.all()[0].name
+                videodir, videofile = os.path.split(videopath)
+                targetdirectory = os.path.join(TARGETPATH, videodir)
+                targetfile = os.path.join(TARGETPATH, videopath)
+                sourcefile = os.path.join(SOURCEPATH, videopath)
+                try:
+                    os.makedirs(targetdirectory)
+                except:
+                    pass
+                print sourcefile, targetfile
+                if os.path.isfile(sourcefile):
+                    user.episode_set.add(episode)
+                    try:
+                        os.symlink(sourcefile, targetfile)
+                    except:
+                        pass
+
