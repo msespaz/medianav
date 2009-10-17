0539b9b64173835a0bbcdbc7bb87b79afd4474f7
andre.boogaards <andre.boogaards@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-10-17 12:33:47 +0000

    [PCH] Added display of movie posters to the PCH movielist, also made the PCH paths configurable in settings.py

--- trunk/server/pch/urls.py
+++ trunk/server/pch/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('pch.views',
     (r'^$', 'popcornhour_home'),
-    (r'^all/(?P<page_number>\d+)/$', 'popcornhour_all'),
+#    (r'^all/(?P<page_number>\d+)/$', 'popcornhour_all'),
+    (r'^all/(?P<page_number>\d+)/(?P<movie_id>\d+)/$', 'popcornhour_all'),
     (r'^detail/(?P<movie_id>\d+)/$', 'popcornhour_detail'),
 )
