06b15e8095ef07c0f599a0ce1f1292ecf6d7e0f1
andre.boogaards <andre.boogaards@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-10-20 18:17:54 +0000

    [PCH] Added filtering by genre

--- trunk/server/pch/views.py
+++ trunk/server/pch/views.py
@@ -10,6 +10,18 @@ def popcornhour_home(request):
     return render_to_response('pch_home.html', locals(), context_instance=RequestContext(request))
 
 def popcornhour_all(request, page_number, movie_id):
+    
+    movies_selection = movies.models.Movie.objects.all()
+    prefix = 'all'
+    return popcornhour_list_movies(request, page_number, movie_id, movies_selection, prefix)
+
+def popcornhour_genre(request, page_number, movie_id, genre_id):
+    genre = movies.models.Genre.objects.get(pk=genre_id)
+    movies_selection = genre.movie_set.all()
+    prefix = 'genre/' + genre_id
+    return popcornhour_list_movies(request, page_number, movie_id, movies_selection, prefix)
+
+def popcornhour_list_movies(request, page_number, movie_id, movies_selection, prefix):
 
     pch_base = settings.MEDIANAV_PCH_MOVIE
 
@@ -17,9 +29,12 @@ def popcornhour_all(request, page_number, movie_id):
     movies_per_page = 10
     start_movie = movies_per_page * (int(page_number)-1)
     end_movie = start_movie + movies_per_page
-    movies_list = movies.models.Movie.objects.all()[start_movie:end_movie]
-    total_movies =  movies.models.Movie.objects.all().count()
+#    movies_list = movies.models.Movie.objects.all()[start_movie:end_movie]
+#    total_movies =  movies.models.Movie.objects.all().count()
+    movies_list = movies_selection[start_movie:end_movie]
+    total_movies = movies_selection.count()
 
+    print prefix
     # Generate previous and next page_number, verify they're within range
     # is_first_page and is_last_page are used in the template to determine
     # whether or not the Up and Down links should be shown.
@@ -59,3 +74,31 @@ def popcornhour_detail(request, movie_id):
     videofile = videodirectory.videofile_set.all()[0]
     return render_to_response('pch_detail.html', locals(), context_instance=RequestContext(request))
 
+def popcornhour_genre_list(request, page_number, genre_id):
+    genres_per_page = 10
+    start_genre = genres_per_page * (int(page_number)-1)
+    end_genre = start_genre + genres_per_page
+    genres_list = movies.models.Genre.objects.all().order_by('name')[start_genre:end_genre]
+    total_genres = movies.models.Genre.objects.all().count()
+
+    total_pages = total_genres / genres_per_page
+    print total_pages, total_genres, genres_per_page
+    if total_genres % genres_per_page != 0:
+        total_pages = total_pages + 1
+    prev_page = int(page_number) - 1
+    next_page = int(page_number) + 1
+    if prev_page < 1:
+        prev_page = 1
+        is_first_page = 1
+    if next_page > total_pages:
+        next_page = total_pages
+        is_last_page = 1
+
+    if int(genre_id) == 0:
+        selected = genres_list[0]
+    else:
+        selected = movies.models.Genre.objects.get(pk=genre_id)
+
+    focus = selected.pk
+
+    return render_to_response('pch_genre.html', locals(), context_instance=RequestContext(request))
