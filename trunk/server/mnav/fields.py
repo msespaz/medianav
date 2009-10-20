d7394592639260fbe87260eea32d17f2b336bd20
AndreFMiller <AndreFMiller@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-10-20 20:56:04 +0000

    Refactoring, moved custom BigInteger field out of movies app to mnav app.
    This is in preparation for further refactoring and the introduction of south migrations

---
+++ trunk/server/mnav/fields.py
@@ -0,0 +1,10 @@
+from django.db.models.fields import IntegerField
+
+class BigIntegerField(IntegerField):
+    """ Defines a custom bigint field """
+    empty_strings_allowed=False
+    def get_internal_type(self):
+        return "BigIntegerField"
+                        
+    def db_type(self):
+        return 'NUMBER(19)' if settings.DATABASE_ENGINE == 'oracle' else 'bigint'
