0899046ca51e248add670b6d55ce463c77acb482
andre.boogaards <andre.boogaards@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-10-24 23:27:24 +0000

    [PCH] Added support for TV episode management, including display of Unwatched eps per user and marking an episode as watched once it plays completely.

--- trunk/server/pch/urls.py
+++ trunk/server/pch/urls.py
@@ -6,5 +6,16 @@ urlpatterns = patterns('pch.views',
     (r'^detail/(?P<movie_id>\d+)/$', 'popcornhour_detail'),
     (r'^genre/(?P<genre_id>\d+)/(?P<page_number>\d+)/(?P<movie_id>\d+)/$', 'popcornhour_genre'),
     (r'^genre_list/(?P<page_number>\d+)/(?P<genre_id>\d+)/$', 'popcornhour_genre_list'),
-    
+
+    (r'^tv/(?P<username>\w+)/shows/(?P<page_number>\d+)/(?P<selected_id>\d+)/$', 'popcornhour_shows_list'),
+    (r'^tv/(?P<username>\w+)/show/(?P<show_id>\d+)/episodes/(?P<page_number>\d+)/(?P<selected_id>\d+)/$', 'popcornhour_show_episodes'),
+    (r'^(?P<type>\w+)/(?P<username>\w+)/(?P<id>\d+)/playlist.jsp$', 'popcornhour_generate_playlist'),
+
+#        (r'^json/(?P<username>\w+)/show/(?P<show_id>\d+)/episodes/$', 'json_show_episodes'),
+
+#    (r'^json/(?P<username>\w+)/episode/(?P<episode_id>\d+)/watched/$', 'json_episode_watched'),
+#    http://192.168.2.100:8000/medianav/pch/tv/all/shows/1/0
+#    http://192.168.2.100:8000/medianav/pch/tv/andre/shows/1/0
+
+
 )
