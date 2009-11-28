bde31667677697d7cd722f8d0e9a54b29792992c
AndreFMiller <AndreFMiller@3ae9964c-60fe-11de-bc6d-35d2ebd5fd4e>
2009-11-28 18:45:35 +0000

    'manage.py movie_scan_dir all' now also checks the database for directories that no longer exists and removes them

--- server/movies/management/commands/movie_scan_dir.py
+++ server/movies/management/commands/movie_scan_dir.py
@@ -2,13 +2,24 @@ from django.core.management.base import LabelCommand
 from movies.utils import import_tmdb, scan_directory
 from django.conf import settings
 import os
+from movies.models import VideoDirectory
 
 class Command(LabelCommand):
     def handle_label(sself, label, **options):
         """ Scan a directory """
         if label == 'all':
+            # Scan all directories in the movie media directory
             for directory in os.listdir(settings.MEDIANAV_MOVIE_DIR):
                 if os.path.isdir(os.path.join(settings.MEDIANAV_MOVIE_DIR, directory)):
                     scan_directory(os.path.join(settings.MEDIANAV_MOVIE_DIR, directory))
+            # Scan the database and remove directories that do not exist
+            for directory in VideoDirectory.objects.all():
+                if not os.path.isdir(os.path.join(settings.MEDIANAV_MOVIE_DIR, directory.name)):
+                    print "DBFIX: Removing Directory %s" % directory.name
+                    for file in directory.movievideofile_set.all():
+                        print "DBFIX:     Removing File %s" % file.name 
+                        file.delete()
+                    directory.delete()
+
         else:
             scan_directory(label)
